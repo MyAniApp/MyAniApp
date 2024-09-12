80e998cd57f44a643cffaee7e9bae1e3d66fa49e
E-boi <E-boi@users.noreply.github.com>
2024-09-12 23:45:11 +0000

    deploy: c4c121566281d2ab87cfc73620cff5fd54748e97

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1358065849"
+    serviceWorkerVersion: "136248592"
   }
 });
