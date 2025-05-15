054ec10b2bf9fccec57c3b59e8d2c6641a8469db
E-boi <eboi.email.official@gmail.com>
2025-05-15 17:01:53 -0400

    Fix android build

---
+++ android/app/src/main/kotlin/com/eboi/myaniapp/myaniapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.eboi.myaniapp.myaniapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/myaniapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.eboi.myaniapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
